ba98af142b5b4213431ffea1a9369c775117c25a
Hariom Kashyap <omkashyap6870@gmail.com>
2025-12-29 21:59:07 +0530

    UI structure complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import Container from "./components/Container"
+import Header from "./components/Header"
+import Weather from "./components/Weather"
+
 const App = () => {
   return (
-    <div></div>
+    <div className="container mx-auto">
+      <Container>
+        <>
+          <Header />
+          <Weather />
+        </>
+      </Container>
+    </div>
   )
 }
+
+export default App
